e8aa00304d5cfe72ea1c760f13909e358933da8a
Akash mishra <mak650650@gmail.com>
2022-11-13 16:18:19 +0530

    whole project is add to repo

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request
+import get_meme
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == "GET":
+        return render_template("index.html")
+    elif request.method == "POST":
+        return render_template("greet.html", name=request.form.get("name","World"))
+
+
+@app.route('/meme', methods=["POST"])
+def meme_func():
+    meme_pic, subreddit = get_meme.get_meme()
+    return render_template("show_meme.html", meme_pic=meme_pic, subreddit=subreddit)
+
+if __name__ =="__main__":
+    app.run(debug=True ,host="0.0.0.0", port=5001)
---
+++ get_meme.py
@@ -0,0 +1,9 @@
+import json
+import requests
+
+def get_meme():
+    url = "https://meme-api.herokuapp.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large =  response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
