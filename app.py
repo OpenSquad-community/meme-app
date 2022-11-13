8278c90df6d2df31d1ce01ed88c562f903fbf623
AbhishekJadhav2002 <www.abhishek3jadhav@gmail.com>
2022-11-13 19:32:14 +0530

    Added PORT in app.run()

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 import get_meme
+import os
 
 app = Flask(__name__)
-
+PORT = int(os.environ.get('PORT', 5000))
 
 @app.route('/', methods=['GET','POST'])
 def home():
@@ -18,4 +19,4 @@ def meme_func():
     return render_template("show_meme.html", meme_pic=meme_pic, subreddit=subreddit)
 
 if __name__ =="__main__":
-    app.run()
+    app.run(port=PORT)
